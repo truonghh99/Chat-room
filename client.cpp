c80c524984d74033d7072d27fb3911b515e3cfbc
Ha Truong <truonghh@beloit.edu>
2019-06-27 18:29:28 +0700

    Usernames added

--- client.cpp
+++ client.cpp
@@ -66,7 +66,6 @@ int main(int arg, char const *argv[]) {
   // Communicate with other clients via server
   std::thread recieve_from_server(server_to_screen, sock);
 
-  // Read from keyboard and send to server
   while (true) {
     std::string temp = "";
     getline (std::cin, temp);
