23b03ebe13e8eb6fee7ab805aac74e1d2ca30527
Ha Truong <truonghh@beloit.edu>
2019-06-27 12:17:19 +0700

    Fix according to comments

--- client.cpp
+++ client.cpp
@@ -10,40 +10,44 @@
 #define SERVER_PORT 9699
 
 int main(int arg, char const *argv[]) {
-	int sock = 0, valread;
-	struct sockaddr_in serv_addr;
-	char buffer[1024] = {0};
-	int test;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    int test;
 
-	// create client socket
-	test = sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (test == -1) {
-		perror("socket failed");
-		exit(-1); 
-	}
-	 serv_addr.sin_family = AF_INET;
-	 serv_addr.sin_port = htons(SERVER_PORT);
+    // Create client socket
+    test = sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (test == -1) {
+        perror("socket failed");
+        exit(-1); 
+    }
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_port = htons(SERVER_PORT);
 
-	// convert address
-	test = inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-	if (test == -1) {
-		perror("invalid address");
-		exit(-1);
-	}
-	test = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	if (test == -1) {
-		perror("connect failed");
-		exit(-1);
-	}
-	while (true) {
-		std::string temp = "";
-		getline (std::cin, temp);
-		if (!temp.empty()) {
-			const char *tempAdd= temp.c_str();
-			send(sock, tempAdd, temp.length(), 0);
-		}
-	}
-	valread = read(sock, buffer, 1024);
-	printf("%s\n", buffer);
-	return 0;
+    // Convert address
+    test = inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    if (test == -1) {
+        perror("invalid address");
+        exit(-1);
+    }
+    test = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (test == -1) {
+        perror("connect failed");
+        exit(-1);
+    }
+
+    // Read from keyboard and send to server
+    while (true) {
+        std::string temp = "";
+        getline (std::cin, temp);
+        if (!temp.empty()) {
+            const char *tempAdd= temp.c_str();
+            send(sock, tempAdd, temp.length(), 0);
+        }
+    }
+
+    // Read from server and print
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -13,62 +13,58 @@ int server_fd, new_socket, valread;
 struct sockaddr_in address;
 int addrlen = sizeof(address);
 int opt = 1;
-char *hello = "Hello from server";
 int test;
 
-void read_print (int socket) {
-	char buffer[1024] = {0};
-	while (true) {
-		int valread = read(socket, buffer, 1024);
-		printf("%s\n", buffer);
-	}
+void worker (int socket) {
+    char buffer[1024] = {0};
+    while (true) {
+        int valread = read(socket, buffer, 1024);
+        printf("%s\n", buffer);
+    }
 }
 
-int main(int argc, char const *argv[]) {	
+int main(int argc, char const *argv[]) {    
+    // Creating socket
+    test = server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (test == -1) {
+        perror("socket failed");
+        exit(-1);
+    }   
 
-	// creating socket
-	test = server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (test == -1) {
-		perror("socket failed");
-		exit(-1);
-	}	
+    // Set socket option
+    test = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (test == -1) {
+        perror("set socket option failed");
+        exit(-1);
+    } 
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons(PORT);
 
-	// set socket option
-	test = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-	if (test == -1) {
-		perror("set socket option failed");
-		exit(-1);
-	} 
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons(PORT);
+    // Attach socket to port
+    test = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    if (test == -1) {
+        perror("bind failed");
+        exit(-1);
+    }
 
-	// attach socket to port
-	test = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-	if (test == -1) {
-		perror("bind failed");
-		exit(-1);
-	}
+    // Waiting for clients to connect
+    test = listen(server_fd, 3);
+    if (test == -1) {
+        perror("listen failed");
+        exit(-1);
+    }
 
-	// listen
-	test = listen(server_fd, 3);
-	if (test == -1) {
-		perror("listen failed");
-		exit(-1);
-	}
+    // Accept and print message from client
+    std::vector<std::thread> threads;
+    while (true) {
+        test = new_socket = accept(server_fd, static_cast<struct sockaddr*>(&address), static_cast<socklen_t*>(&addrlen));
+        if (test == -1) {
+            perror("accept failed");
+            exit(-1);
+        }
+        threads.emplace_back(std::thread(worker, new_socket));
+    }
 
-	// accept
-	std::vector<std::thread> threads;
-	while (true) {
-		test = new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-		if (test == -1) {
-			perror("accept failed");
-			exit(-1);
-		}
-		threads.emplace_back(std::thread(read_print, new_socket));
-	}
-
-	send(new_socket, hello, strlen(hello), 0);
-	printf("Hello message sent");
-	return 0;
+    return 0;
 }
